4c6e2432905ad75a5be804f546c2f6c7fa514254
KurtOsorio <kurtjehu10@gmail.com>
2023-05-12 13:49:33 +0800

    modified files

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 const sections = document.querySelectorAll("section[id]");
@@ -31,3 +49,31 @@ function navHighlighter() {
     }
   });
 }
+
+// Modal
+(function() {
+  emailjs.init('lF1m_Byuvj_hi4UOO');
+})();
+
+document.getElementById('my-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // get the form data
+  var formData = {
+      from_name: document.getElementById('from_name').value,
+      from_email: document.getElementById('from_email').value,
+      to_name: document.getElementById('to_name').value,
+      to_email: document.getElementById('to_email').value,
+      subject: document.getElementById('subject').value,
+      message: document.getElementById('message').value
+  };
+
+  // send the email
+  emailjs.send('service_8my8f57', 'template_v1ny5ha', formData)
+      .then(function(response) {
+          alert('Sent Successfully')
+          console.log('SUCCESS!', response.status, response.text);
+      }, function(error) {
+          console.log('FAILED...', error);
+      });
+});
